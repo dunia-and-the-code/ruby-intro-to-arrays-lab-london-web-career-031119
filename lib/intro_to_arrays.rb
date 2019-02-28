6dc6d658c0b5de0cdbae9ce49d8ed266c028b7d5
Dunia Al-Baghdadi <dunia-al-baghdadi@hotmail.com>
2019-02-28 10:57:49 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -5,3 +5,7 @@ end
 def array_with_two_elements
   return @my_two_array = [ "red", "blue"]
 end
+
+def indexing
+  return @taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
+end
